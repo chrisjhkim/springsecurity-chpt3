a9af7747522ecd5e98e483cf1419234b535098e0
chrisjhkim <chris1108g@gmail.com>
2023-11-18 18:25:27 +0900

    Initial Commit

---
+++ src/test/java/com/chrisjhkim/ssia/SpringSecurityInActionChpater3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisjhkim.ssia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInActionChpater3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
