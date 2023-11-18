1789842f95a2932ce13b5e13565587cceaddf3f2
chrisjhkim <chris1108g@gmail.com>
2023-11-18 23:57:08 +0900

    3.3.3 UserDetailsManager 계약 구현

--- src/main/java/com/chrisjhkim/ssia/config/CustomInMemoryUserDetailsServiceProjectConfig.java
+++ src/main/java/com/chrisjhkim/ssia/config/CustomInMemoryUserDetailsServiceProjectConfig.java
@@ -1,19 +1,24 @@
 package com.chrisjhkim.ssia.config;
 
+import com.chrisjhkim.ssia.model.User;
+import com.chrisjhkim.ssia.service.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import javax.sql.DataSource;
+import java.util.List;
 
-@Configuration
-public class ProjectConfig {
+//@Configuration
+public class CustomInMemoryUserDetailsServiceProjectConfig {
+
 	@Bean
-	public UserDetailsService userDetailsService(DataSource dataSource){
-		return new JdbcUserDetailsManager(dataSource);
+	public UserDetailsService userDetailsService(){
+		UserDetails user = new User("john", "12345", "read");
+		List<UserDetails> users = List.of(user);
+		return new InMemoryUserDetailsService(users);
 	}
 
 
